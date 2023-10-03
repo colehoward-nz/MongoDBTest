19ea97faa37f190c34bda8beefe4d751d46f7d1f
Cole <74393866+colehoward-nz@users.noreply.github.com>
2023-10-04 12:55:01 +1300

    inital push

---
+++ src/main/java/me/cole/mongodbtest/MongoDBTest.java
@@ -0,0 +1,17 @@
+package me.cole.mongodbtest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MongoDBTest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        //test
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
