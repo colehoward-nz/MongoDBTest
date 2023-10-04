d2cf4dc514aaf5442fe4d3234331f3a407b48566
Cole <74393866+colehoward-nz@users.noreply.github.com>
2023-10-04 13:03:13 +1300

    maybe

--- src/main/java/me/cole/mongodbtest/MongoDBTest.java
+++ src/main/java/me/cole/mongodbtest/MongoDBTest.java
@@ -1,12 +1,26 @@
 package me.cole.mongodbtest;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.function.Consumer;
 
 public final class MongoDBTest extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        //test
+        MongoClient mongoClient = MongoClients.create("mongodb+srv://poo:Uncrackable!DWLn4CTVLWXeI5a0@kitpvpcluster.avyl0on.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp");
+        MongoDatabase database = mongoClient.getDatabase("sample_weatherdata");
+        MongoCollection<Document> collection = database.getCollection("data");
+
+        collection.find().forEach((Consumer<Document>) document -> {
+            Object test = document.get("_id");
+            System.out.println(test);
+        });
 
     }
 
