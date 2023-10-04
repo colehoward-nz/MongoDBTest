735e7870300537b1f60cbf6774dad084afa51b00
Cole <74393866+colehoward-nz@users.noreply.github.com>
2023-10-04 13:51:17 +1300

    playerdata schema

--- src/main/java/me/cole/mongodbtest/MongoDBTest.java
+++ src/main/java/me/cole/mongodbtest/MongoDBTest.java
@@ -1,27 +1,31 @@
 package me.cole.mongodbtest;
 
+import com.mongodb.*;
 import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.function.Consumer;
+import java.util.HashMap;
+import java.util.UUID;
 
 public final class MongoDBTest extends JavaPlugin {
+    private MongoCollection<Document> collection;
+    private HashMap<UUID,PlayerData> playerDataHashMap;
 
     @Override
     public void onEnable() {
-        MongoClient mongoClient = MongoClients.create("mongodb+srv://poo:Uncrackable!DWLn4CTVLWXeI5a0@kitpvpcluster.avyl0on.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp");
-        MongoDatabase database = mongoClient.getDatabase("sample_weatherdata");
-        MongoCollection<Document> collection = database.getCollection("data");
+        this.playerDataHashMap = new HashMap<>();
+        this.getServer().getPluginManager().registerEvents(this, this);
 
-        collection.find().forEach((Consumer<Document>) document -> {
-            Object test = document.get("_id");
-            System.out.println(test);
-        });
+        String uri = "mongodb://Admin:admin@mongodb-shard-00-00-altt9.mongodb.net:27017,mongodb-shard-00-01-altt9.mongodb.net:27017,mongodb-shard-00-02-altt9.mongodb.net:27017/admin?replicaSet=MongoDB-shard-0&ssl=true";
+        MongoClientURI clientURI = new MongoClientURI(uri);
+        MongoClient mongoClient = new MongoClient(clientURI);
 
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("MongoDB");
+        collection = mongoDatabase.getCollection("Minecraft");
+
+        this.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[MONGODB] Database Connected");
     }
 
     @Override
